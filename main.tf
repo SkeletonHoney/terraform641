888a807f99aba1a3c21fe0ebb79a0ce16946f931
SkeletonHoney <ryanerogers5@gmail.com>
2020-03-04 01:17:42 +0000

    main.tf now works

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
   region = "us-east-1"
+  profile= "rogers_ryan"
 }
 
-resource "aws_instance" "web" {
-  ami           = "ami-002cc39e7bf021a77"
+resource "aws_instance" "example" {
+  ami           = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
-
   tags = {
-    Name = "rrogers_test"
+    Name = "rrogers_tf_test"
   }
 }
