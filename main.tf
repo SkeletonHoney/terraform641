dca2e84653533a2f345661c77c157d211696412e
SkeletonHoney <ryanerogers5@gmail.com>
2020-03-04 02:25:09 +0000

    added key_name

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0a887e401f7654935"
   instance_type = "t2.micro"
+  key_name	= "rrogers_NV"
   tags = {
     Name = "rrogers_tf_test"
   }
